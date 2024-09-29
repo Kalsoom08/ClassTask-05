70b5c87668be282f84301947540d479c6176e358
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-09-29 15:56:13 +0500

    Condition is Added to avoid submitting empty form

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let ageFinder = document.getElementById("age-calculator");
-ageFinder.addEventListener('click',function(){
-  let dateInput = document.getElementById("date");
+ageFinder.addEventListener('click',function(event){
+  event.preventDefault();
+  let dateInput = document.getElementById("date").value;
   if(dateInput === ""){
     alert("Enter Date First");
-  
   }
+else {
+    let getCurrentDate = new Date();
+    let birthDate = new Date(dateInput);
+    
+    console.log("Current Date:", getCurrentDate);
+    console.log("Birth Date:", birthDate);
+    
+   
+  }
+});
 
-})
+
