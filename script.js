6412867570514700f35865f4d7487c9ad8674fc1
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-09-29 15:50:03 +0500

    Get Elements using Id

---
+++ script.js
@@ -0,0 +1,9 @@
+let ageFinder = document.getElementById("age-calculator");
+ageFinder.addEventListener('click',function(){
+  let dateInput = document.getElementById("date");
+  if(dateInput === ""){
+    alert("Enter Date First");
+  
+  }
+
+})
